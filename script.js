4d64c9f515d6e98ec9e612a9b2dc15a53b2198b4
Rayhona <rayhonamirzoyeva.ru>
2024-01-11 15:42:29 +0500

    kddjqjd

--- script.js
+++ script.js
@@ -10,9 +10,72 @@ let shirt = {
     }
 }
 
-console.log(shirt.sayHello());
+//console.log(shirt.sayHello());
 // console.log(shirt['color']);
 
- function sayHello(){
+// function sayHello(){
+  //   return " hello everyOne "
+//}
 
-}
+//console.log(sayHello());
+
+   // ? Malumot turlari
+// Number
+// boolean
+// string
+// undefined
+// null
+// object
+
+let str1 = "Bittali Qo`shtirnoq";
+// Bittali Qo`shtirnoq
+// 012345678910 
+ 
+let str2 ='bektik orqali string yasash'
+
+// ? String property
+console.log(str.length);
+
+//? String methods
+// charAt()
+console.log(str.charAt(6));
+
+// At()
+console.log(str.At(6));
+
+// slice()
+
+let str = 'Bittali qo`shtirnoq kjhcbwjcbhkjwuc';
+console.log(str[0]);
+
+let slicedStr = str.slice(-10, -1)
+console.log(slicedStr);
+
+// Substring()
+let newSub = str.substring(8, str [length -1])
+console.log(newSub);
+
+// Substr()
+
+ let substrNew = str.substr(8,5)
+ console.log(substrNew);
+
+   // toLowerCase()
+   console.log(str.loLowerCase());
+
+   // toUpperCase
+   console.log(str.toUpperCase());
+
+   // concat
+
+   let a = "hello         ";
+   let  b = "world         ";
+   let  z = "war";
+   let  x = a.concat(b,z)
+   console.log(a + b + z);
+   
+   // trim()
+
+   console.log(a.length);
+
+   
