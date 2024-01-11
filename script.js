31badf31f85317d40337ebd253e9bb7dd346ac32
Rayhona <rayhonamirzoyeva.ru>
2024-01-11 15:03:11 +0500

    add js

---
+++ script.js
@@ -0,0 +1,18 @@
+let shirtColor = "red";
+shirtSize = "xxl"; 
+
+let shirt = {
+    color:"red",
+    size: "xxl",
+    // Method
+    sayHello: function (){
+        return "hiiiii"
+    }
+}
+
+console.log(shirt.sayHello());
+// console.log(shirt['color']);
+
+ function sayHello(){
+
+}
