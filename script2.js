4b4e0788be6a3e1951d9fd791baf575e0975d118
Rayhona <rayhonamirzoyeva.ru>
2024-01-13 15:11:50 +0500

    mkwmmd

---
+++ script2.js
@@ -0,0 +1,40 @@
+let str = "salom";
+
+function sliceFunc(str1, age) {
+    // ? includes()
+
+    // let incStr = str.includes("nima")
+    // ? indexOf
+    // let indexStr = str1.indexOf("dunyo")
+    // let arrFruit = ["banana", "apple", "orange", "lemon"]
+    // let indexArr = arrFruit.indexOf("apple")
+
+    // ? if else shart operatori
+    // if(str1.length > 10) {
+    //     return "string uzuun";
+    // } else {
+    //     return "katta"
+    // }
+
+    // if (age < 16) {
+    //     return "uzur yoshingiz kichik";
+    // } else if(age >= 50) {
+    // } else {
+    //     return "marhamat hush kelibsiz"
+    // }
+
+
+
+    // 90+   A+
+    // 70 va 90 oralig'ida A
+    // 60 va 70 oralig'ida B
+    // 50 va 60 oralig'ida C
+    // 50 dan kichik F  yani yiqilgan
+
+
+}
+
+console.log(sliceFunc(str, 50));
+
+
+let exam  
